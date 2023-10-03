483c6081a2a338d45095f833fd6885d41d89e3d7
daviddpi <piscopodavid97@gmail.com>
2023-10-03 17:38:36 +0200

    feat: install font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import { Heading } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
 
 export default function Index() {
-    return <Text color="blue" fontWeight="bold">Hello, Next.js & Chakra UI!</Text>
-  }
+  return (
+    <>
+      <Heading fontSize="4rem">Streaming<sup>TV</sup></Heading>
+      <Text color="blue" fontWeight="bold" fontSize="1.6rem">
+        Hello, Next.js & Chakra UI!
+      </Text>
+    </>
+  );
+}

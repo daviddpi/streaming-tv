20f2f8655a13aaea07fbf7e1c18dcbcb4e6c1834
daviddpi <piscopodavid97@gmail.com>
2023-10-03 17:53:52 +0200

    feat: colors

---
+++ src/lib/chakra/theme/colors.ts
@@ -0,0 +1,19 @@
+export const colors = {
+    background: "#0D1821",
+    primary: "#5762D5",
+    secondary: "#f0ebf7",
+    tertiary: "#625e65",
+    white: "#ffffff",
+    black: "#000000",
+    red: "#ea4335",
+    yellow: "#fbbc04",
+    blue: {
+        normal: "#1a73e8",
+        dark: "#174ea6"
+    },
+    green: {
+        normal: "#34a853",
+        dark: "#0d652d"
+    }
+
+}

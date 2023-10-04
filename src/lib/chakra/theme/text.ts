1c258fb62b7edc992f0efb7a23699f37a18cdee1
daviddpi <piscopodavid97@gmail.com>
2023-10-04 10:10:04 +0200

    fix: line height text

--- src/lib/chakra/theme/text.ts
+++ src/lib/chakra/theme/text.ts
@@ -5,35 +5,42 @@ export const text = {
     fontSize: "4rem",
     fontFamily: fonts.heading,
     fontWeight: "bold",
+    lineHeight: "4rem"
   },
   h1: {
     fontSize: "2.8rem",
     fontFamily: fonts.body,
     fontWeight: 700,
+    lineHeight: "2.8rem"
   },
   h2: {
     fontSize: "2.4rem",
     fontFamily: fonts.body,
     fontWeight: 700,
+    lineHeight: "2.4rem"
   },
   h3: {
     fontSize: "2rem",
     fontFamily: fonts.body,
     fontWeight: 700,
+    lineHeight: "2rem"
   },
   body1: {
     fontSize: "1.4rem",
     fontFamily: fonts.body,
     fontWeight: 500,
+    lineHeight: "1.4rem"
   },
   body2: {
     fontSize: "1.6rem",
     fontFamily: fonts.body,
     fontWeight: 500,
+    lineHeight: "1.6rem"
   },
   button: {
     fontSize: "1.9rem",
     fontFamily: fonts.body,
     fontWeight: 600,
+    lineHeight: "1.9rem"
   },
 };

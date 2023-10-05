eb21ca911a3853674a9f7da61bc83cfbc76f38d0
daviddpi <piscopodavid97@gmail.com>
2023-10-05 16:55:37 +0200

    feat: button

---
+++ src/components/Button/ButtonProps.ts
@@ -0,0 +1,13 @@
+import { ChakraProps, ThemingProps } from "@chakra-ui/react";
+import type { IconName, IconSize } from "../Icon/IconProps";
+
+export interface ButtonProps extends ChakraProps, ThemingProps {
+  label?: string;
+  icon?: IconName;
+  iconSize?: IconSize;
+  iconLeft?: boolean;
+  iconRight?: boolean;
+  tag?: "button" | "link";
+  href?: string;
+  external?: boolean;
+}
---
+++ src/lib/chakra/components/button.ts
@@ -0,0 +1,88 @@
+import { defineStyle, defineStyleConfig } from "@chakra-ui/react";
+
+const baseLink = {
+  p: 0,
+  border: "none",
+  _hover: {
+    TextDecoder: "none",
+  },
+};
+
+export const Button = defineStyleConfig({
+  baseStyle: defineStyle({
+    borderRadius: "0.8rem",
+    textDecoration: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "0.9rem",
+  }),
+  sizes: {
+    base: {
+      w: "fit-content",
+      h: "auto",
+      padding: "1.1rem 2.4rem",
+    },
+  },
+  variants: {
+    primary: {
+      color: "inherit",
+      bg: "primary.default",
+      _hover: {
+        bg: "primary.hover",
+      },
+      _active: {
+        bg: "primary.active",
+      },
+    },
+    secondary: {
+      color: "inherit",
+      bg: "secondary.default",
+      _hover: {
+        bg: "secondary.hover",
+      },
+      _active: {
+        bg: "secondary.default",
+      },
+    },
+    tertiary: {
+      border: "1px solid",
+      borderColor: "primary.default",
+      color: "primary.default",
+      bg: "transparent",
+      _hover: {
+        color: "white",
+        bg: "primary.default",
+      },
+      _active: {
+        bg: "primary.active",
+      },
+    },
+    icon: {
+      p: 0,
+      color: "black",
+      _hover: {
+        color: "tertiary",
+      },
+    },
+    linkPrimary: {
+      ...baseLink,
+      color: "black",
+      _hover: {
+        color: "tertiary",
+      },
+    },
+    linkSecondary: {
+      ...baseLink,
+      color: "white",
+      _hover: {
+        color: "secondary.default",
+      },
+    },
+  },
+
+  defaultProps: {
+    variant: "tertiary",
+    size: "base",
+  },
+});
